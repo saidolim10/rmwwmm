128894775c0e3abb070cd05d57e86686a3ec2551
saidolim10 <git001hub@gmail.com>
2025-03-25 11:16:05 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let name1 = prompt("Ismingizni kiriting");
+let harf1 = prompt("Biror harf kiriting");
+if (name1.includes(harf1)) {
+    alert("Sizning ismingizda " + harf1 + " harfi bor");
+}
+else {
+    alert("Sizning ismingizda " + harf1 + " harfi yo'q");
+}
